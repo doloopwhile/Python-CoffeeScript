fe888c456b05be16dc923aaffbbc0d4b17f77eac
OMOTO Kenji <doloopwhile@gmail.com>
2013-09-21 18:49:43 +0900

    issue #8 Incremented version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open('README.rst', encoding='ascii') as fp:
 
 setup(
     name='CoffeeScript',
-    version='1.0.7',
+    version='1.0.8',
     author='OMOTO Kenji',
     description='A bridge to the JS CoffeeScript compiler',
 
